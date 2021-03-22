4f569d248e6ba5387f3b0154f41db33d93c2f312
liaukx-tarc <liaukx@student.tarc.edu.my>
2021-03-23 01:01:03 +0800

    Done Main Menu, Fix Tower Build out of the map
    - Done Main Menu
    - Fix Tower Build out of the map

--- CGP_Assignment/Code/Level/MainMenu.h
+++ CGP_Assignment/Code/Level/MainMenu.h
@@ -7,13 +7,17 @@
 #include "GameState.h"
 #include "../Object/Button.h"
 
+#define TITLE1 "TOWER"
+#define TITLE2 "DEFENSE"
+#define MADEBY "Made by Voong Meng & Kai Xian Team"
+
 class MainMenu : public GameState
 {
 public:
 	MainMenu();
 	~MainMenu();
 
-	HRESULT hr[5];
+	HRESULT hr[7];
 
 	void init();
 	void fixUpdate();
@@ -27,14 +31,18 @@ public:
 
 	LPD3DXSPRITE sprite;
 	LPD3DXFONT font;
+	LPD3DXFONT title;
+	LPD3DXFONT madeBy;
 
 	LPDIRECT3DTEXTURE9 buttonTexture;
 	LPDIRECT3DTEXTURE9 background;
 
 	RECT backgroundRect;
+	RECT titleRect1;
+	RECT titleRect2;
+	RECT madeByRect;
 
 	std::vector<Button*> buttonList;
-	
 };
 
 #endif //!MAIN_MENU
