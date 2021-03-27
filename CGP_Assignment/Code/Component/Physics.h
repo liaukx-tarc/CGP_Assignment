e7714d7c9bbb0b965dad1f4512ca5daf53c80b39
FongVM <fongvm-wm19@student.tarc.edu.my>
2021-03-27 10:27:50 +0800

    Projectile
    Still need to debug

--- CGP_Assignment/Code/Component/Physics.h
+++ CGP_Assignment/Code/Component/Physics.h
@@ -1,6 +1,8 @@
 #ifndef PHYSICS
 #define PHYSICS
-#include <dinput.h>
+
+#include <vector>
+#include <d3dx9.h>
 
 class Physics
 {
@@ -12,6 +14,31 @@ private:
 public:
 	static Physics* getInstance();
 	static void releaseInstance();
+
+	void init();
+	void draw();
+	void update();
+	void release();
+	void projectile(D3DXVECTOR3 towerPosition, D3DXVECTOR3 enemyPosition);
+
+	HRESULT hr[2];
+	LPD3DXSPRITE sprite;
+	LPDIRECT3DTEXTURE9 projectileTexture;
+	D3DXMATRIX mat;
+	D3DXVECTOR2 scaling;
+	RECT projectileRect;
+	D3DXVECTOR3 totalDistance;
+	D3DXVECTOR3 currentDistance;
+	float tDistance;
+	float cDistance;
+	D3DXVECTOR3 projectilePosition;
+	D3DXVECTOR3 drawPosition;
+	bool isComplete;
+	std::vector<D3DXVECTOR3> towerPositionList;
+	std::vector<D3DXVECTOR3> enemyPositionList;
+	std::vector<D3DXVECTOR3> projectilePositionList;
+	int timer;
+
 };
 
 #endif // !DIRECT_INPUT
